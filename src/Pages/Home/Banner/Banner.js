fd18cb8e25e64870cfd3f77b40942db6a2d0198f
Your Name <you@example.com>
2023-07-25 20:05:56 +0600

    Pop image out of the section in Appointment section

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import chair from "../../../assets/images/chair.png";
+import bg from "../../../assets/images/bg.png";
+import PrimaryButton from '../../../Components/PrimaryButton/PrimaryButton';
 
 const Banner = () => {
     return (
-        <div className="hero">
+        <div className="hero"
+            style={{
+                background: `url(${bg})`
+            }}
+        >
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} className="lg:w-1/2 rounded-lg shadow-2xl" />
                 <div>
                     <h1 className="text-5xl font-bold text-green-600">Treatment Chair</h1>
                     <p className="py-4 text-sm font-semibold">Allow only patient in treatment room,Visitors wait in visitor room.Serial maintain mendatory.Hygienic profer uniform & Clean environment compulsory & request to all for maintain please.</p>
-                    <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white font-bold">Get Started</button>
+                    <PrimaryButton>Appointment</PrimaryButton>
                 </div>
             </div>
         </div>
